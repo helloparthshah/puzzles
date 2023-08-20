72640e524077ce0fc16480d8bf8f75f75789d647
kunpai <kunpai@ucdavis.edu>
2023-08-20 09:24:25 -0700

    Added a lock SVG to show unsolved puzzles

--- components/topbar.js
+++ components/topbar.js
@@ -16,6 +16,11 @@ export default function Topbar() {
         <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
         <polyline points="22 4 12 14.01 9 11.01"></polyline>
     </svg>
+
+    const lockedSVG = <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-lock-fill" strokeWidth="2" viewBox="0 0 20 20">
+        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+    </svg>
+
     const solved = true;
     return (
         <Container className="flex-column w-25 h-100 bg-darker" style={{ position: "fixed" }}>
@@ -27,7 +32,10 @@ export default function Topbar() {
                         {solvedSVG}
                     </Tab>
                     <Tab value="tab2" >Puzzle 2</Tab>
-                    <Tab value="tab3" disabled>Puzzle 3</Tab>
+                    <Tab value="tab3" disabled>
+                        {"Puzzle 3 "}
+                        {lockedSVG}
+                    </Tab>
                     <Tab value="tab4" disabled>Puzzle 4</Tab>
                 </TabList>
             </div>
