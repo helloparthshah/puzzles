b2b1ca3c3b74ef30641b15e496c1c81656fa991f
Parth Shah <helloparthshah@gmail.com>
2023-08-21 00:26:16 -0700

    fixed bug with answers
    Co-authored-by: kunpai <kunpai@ucdavis.edu>

--- src/app/api/checkAnswer/route.js
+++ src/app/api/checkAnswer/route.js
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server"
 export async function POST(request, context) {
     // get id from body
     const { id, answer, world } = await request.json()
-    let worldJson = await import(`../puzzles/${world}.json`)
-    let puzzles = JSON.parse(JSON.stringify(worldJson))
-    for (const puzzle of puzzles.puzzles) {
+    console.log(id, answer, world)
+    const worldJson = await import(`../puzzles/${world}.json`)
+    let puzzles = JSON.parse(JSON.stringify(worldJson.puzzles))
+    for (const puzzle of puzzles) {
         if (puzzle.id === id) {
             if (puzzle.answer.toLowerCase() === answer.toLowerCase()) {
                 return NextResponse.json(true)
--- src/app/api/getAllPuzzlesForWorld/route.js
+++ src/app/api/getAllPuzzlesForWorld/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 export async function POST(request, context) {
     const { world } = await request.json()
     const worldFile = await import(`../puzzles/${world}.json`)
-    let puzzles = worldFile.puzzles
+    let puzzles = JSON.parse(JSON.stringify(worldFile.puzzles))
     for (let i = 0; i < puzzles.length; i++) {
         delete puzzles[i].answer
     }
